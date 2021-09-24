199ed5b5fe4e5fd48f05c05134d2da51cbdae72c
Nick Rout <nickrout@google.com>
2021-09-24 11:26:57 +0200

    Update Compose snapshot, use material3 Surface, remove material

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-// TODO: Replace with material3 Surface when available in snapshot
-import androidx.compose.material.Surface
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
