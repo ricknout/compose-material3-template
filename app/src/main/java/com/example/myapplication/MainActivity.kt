8027764b57500194fed3320210bb37a4163228de
Nick Rout <nickrout@google.com>
2021-10-08 14:45:21 +0200

    Update implementation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -17,8 +17,11 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       MyApplicationTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        // A surface container using the 'primaryContainer' color from the theme
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.primaryContainer
+        ) {
           Greeting("Android")
         }
       }
